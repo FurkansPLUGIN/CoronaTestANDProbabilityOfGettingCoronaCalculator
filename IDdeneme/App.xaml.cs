da73925f14be15fd89c88abe14b7f943227823ea
Furkan Demirel <46024787+FurkansPLUGIN@users.noreply.github.com>
2021-03-14 16:09:54 +0300

    code

---
+++ IDdeneme/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IDdeneme
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            Device.SetFlags(new string[] { "MediaElement_Experimental" });
+
+            MainPage = new Tabbed_pageControl();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
